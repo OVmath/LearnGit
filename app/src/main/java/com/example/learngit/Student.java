d7cd55c877c0288248b9ab1c34b381fb58f12224
tuan61 <leminhtuan0601@gmail.com>
2023-06-18 09:25:33 +0700

    Initialize Android Project

---
+++ app/src/main/java/com/example/learngit/Student.java
@@ -0,0 +1,10 @@
+package com.example.learngit;
+
+import java.util.Date;
+
+public class Student {
+    private String studentID;
+    private String fullName;
+    private float gpa;
+    private Date birthday;
+}
