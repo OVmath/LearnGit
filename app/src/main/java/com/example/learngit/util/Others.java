64c38ccd09841592bec1ce1158884efb4915171b
tuan61 <leminhtuan0601@gmail.com>
2023-06-21 18:34:21 +0700

    Add package util

---
+++ app/src/main/java/com/example/learngit/util/Others.java
@@ -0,0 +1,13 @@
+package com.example.learngit.util;
+
+public class Others {
+    private String something;
+
+    public String getSomething() {
+        return something;
+    }
+
+    public void setSomething(String something) {
+        this.something = something;
+    }
+}
