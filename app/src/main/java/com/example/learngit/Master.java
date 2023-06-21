36163d94f961e8adf9d46974d57a75de413f826d
tuan61 <leminhtuan0601@gmail.com>
2023-06-21 10:09:23 +0700

    Add update Master

---
+++ app/src/main/java/com/example/learngit/Master.java
@@ -0,0 +1,13 @@
+package com.example.learngit;
+
+public class Master {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
