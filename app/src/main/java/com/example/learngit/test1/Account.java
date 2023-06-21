e950fc73c0f66d8c5c4aaea3d7b2d88514d5fdbc
tuan61 <leminhtuan0601@gmail.com>
2023-06-21 21:38:05 +0700

    Add Account

---
+++ app/src/main/java/com/example/learngit/test1/Account.java
@@ -0,0 +1,31 @@
+package com.example.learngit.test1;
+
+public class Account {
+    private String email;
+    private int id;
+    private String username;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
