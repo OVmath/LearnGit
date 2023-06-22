61ac320e3ee19b6adb0be8dad32bdc4100dfd510
tuan61 <leminhtuan0601@gmail.com>
2023-06-22 17:32:01 +0700

    Update User

--- app/src/main/java/com/example/learngit/data/User.kt
+++ app/src/main/java/com/example/learngit/data/User.kt
@@ -1,9 +1,10 @@
 package com.example.learngit.data
 
 data class User (
-    val id:Int,
+    val id:Long,
     val firstName:String,
     val lastName:String,
     val email:String
+    // This is an online comment
 )
 
